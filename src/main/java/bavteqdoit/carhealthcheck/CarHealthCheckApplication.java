31cea36d6b1442499a173d70c8386aeb832aeb57
BavteqDoIT <125410325+BavteqDoIT@users.noreply.github.com>
2025-03-16 19:04:03 +0100

    Initial commit

---
+++ src/main/java/bavteqdoit/carhealthcheck/CarHealthCheckApplication.java
@@ -0,0 +1,13 @@
+package bavteqdoit.carhealthcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarHealthCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarHealthCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bavteqdoit/carhealthcheck/CarHealthCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package bavteqdoit.carhealthcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarHealthCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
