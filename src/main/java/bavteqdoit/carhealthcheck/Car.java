1592cc7e517f205c94cc1fdaed769b12c1fa46b6
BavteqDoIT <bartek.adamek@onet.pl>
2025-03-17 14:23:57 +0100

    Added Car model for vehicle representation

---
+++ src/main/java/bavteqdoit/carhealthcheck/Car.java
@@ -0,0 +1,62 @@
+package bavteqdoit.carhealthcheck;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@RequiredArgsConstructor
+public class Car {
+    private final Long id;
+    private final Brand brand;
+    private final Model model;
+    private final Integer year;
+    private final Color color;
+    private final Double engineCapacity;
+    private final EngineType engineType;
+    private final GearType gearType;
+    private final BigDecimal price;
+    private final BodyType bodyType;
+    private final Long mileage;
+    private final MileageUnit mileageUnit;
+    private final Boolean isAccidentFree;
+    private final Integer doors;
+    private final Integer seats;
+    private final String vin;
+    private final LocalDate registrationDate;
+    private final SteeringSide steeringSide;
+
+    public static enum Brand {
+        OPEL, SKODA, MERCEDES, BMW, MINICOOPER, AUDI
+    }
+
+    public static enum Model {
+        ASTRA, CORSA, MOKKA, FABIA, OCTAVIA, SUPERB, VITO, SLK, GT, SERIES3, SERIES5, SERIES7, CABRIO, CLUBMAN, COUNTRYMAN, A3, A5, S3
+    }
+
+    public static enum Color {
+        BLUE, RED, YELLOW, GREEN, BLACK, WHITE, ORANGE, GOLD, SILVER, BROWN, MAGENTA, PURPLE, PINK
+    }
+
+    public static enum EngineType {
+        PETROL, PETROL_CNG, PETROL_LPG, DIESEL, ELECTRIC, ETHANOL, HYBRID, PLUG_IN_HYBRID, HYDROGEN
+    }
+
+    public static enum GearType {
+        AUTOMATIC, MANUAL
+    }
+
+    public static enum BodyType {
+        SEDAN, HATCHBACK, WAGON, COUPE, CONVERTIBLE, SUV, CROSSOVER, PICKUP, MINIVAN, VAN, OFF_ROAD, ROADSTER
+    }
+
+    private static enum MileageUnit {
+        KILOMETERS, MILES
+    }
+
+    private static enum SteeringSide {
+        RIGHT, LEFT
+    }
+}
