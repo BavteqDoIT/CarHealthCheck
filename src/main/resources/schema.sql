88b00b723576a690448b7ca81388fde264ef7c0e
BavteqDoIT <bartek.adamek@onet.pl>
2025-07-23 19:04:22 +0200

    Switched database from H2 to PostgreSQL

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,41 +1,41 @@
 CREATE TABLE IF NOT EXISTS brand (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    brand_name VARCHAR(255) NOT NULL
+                                     id BIGSERIAL PRIMARY KEY,
+                                     brand_name VARCHAR(255) NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS model_type (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    model_type VARCHAR(255) NOT NULL,
+                                          id BIGSERIAL PRIMARY KEY,
+                                          model_type VARCHAR(255) NOT NULL,
     brand_id BIGINT NOT NULL,
     FOREIGN KEY (brand_id) REFERENCES brand(id) ON DELETE CASCADE
     );
 
 CREATE TABLE IF NOT EXISTS color (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    color VARCHAR(255) NOT NULL
+                                     id BIGSERIAL PRIMARY KEY,
+                                     color VARCHAR(255) NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS engine_type (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    engine_type VARCHAR(255) NOT NULL
+                                           id BIGSERIAL PRIMARY KEY,
+                                           engine_type VARCHAR(255) NOT NULL
     );
 
 CREATE TABLE model_engine_type (
-    model_type_id BIGINT NOT NULL,
-    engine_type_id BIGINT NOT NULL,
-    PRIMARY KEY (model_type_id, engine_type_id),
-    FOREIGN KEY (model_type_id) REFERENCES model_type(id) ON DELETE CASCADE,
-    FOREIGN KEY (engine_type_id) REFERENCES engine_type(id) ON DELETE CASCADE
+                                   model_type_id BIGINT NOT NULL,
+                                   engine_type_id BIGINT NOT NULL,
+                                   PRIMARY KEY (model_type_id, engine_type_id),
+                                   FOREIGN KEY (model_type_id) REFERENCES model_type(id) ON DELETE CASCADE,
+                                   FOREIGN KEY (engine_type_id) REFERENCES engine_type(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS body_type (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    body_type VARCHAR(255) NOT NULL
+                                         id BIGSERIAL PRIMARY KEY,
+                                         body_type VARCHAR(255) NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS car (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+                                   id BIGSERIAL PRIMARY KEY,
+                                   name VARCHAR(255) NOT NULL,
     brand_id BIGINT NOT NULL,
     model_type_id BIGINT NOT NULL,
     color_id BIGINT NOT NULL,
