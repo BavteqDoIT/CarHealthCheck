b8522da770792a45c0899b02abc06fec658be0b1
BavteqDoIT <bartek.adamek@onet.pl>
2025-09-25 20:26:34 +0200

    Added min max value to paintcheck form

--- src/main/java/bavteqdoit/carhealthcheck/model/PaintCheck.java
+++ src/main/java/bavteqdoit/carhealthcheck/model/PaintCheck.java
@@ -49,50 +49,75 @@ public class PaintCheck {
     @OneToMany(mappedBy = "paintCheck", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<PaintDamage> damages = new ArrayList<>();
 
-    private Integer hoodThickness;
-    private Integer roofThickness;
-    private Integer frontLeftDoorThickness;
-    private Integer rearLeftDoorThickness;
-    private Integer frontRightDoorThickness;
-    private Integer rearRightDoorThickness;
-    private Integer trunkThickness;
-    private Integer pillarALeftThickness;
-    private Integer pillarARightThickness;
-    private Integer pillarBLeftThickness;
-    private Integer pillarBRightThickness;
-    private Integer pillarCLeftThickness;
-    private Integer pillarCRightThickness;
-    private Integer frontLeftWheelArchThickness;
-    private Integer frontRightWheelArchThickness;
-    private Integer rearLeftWheelArchThickness;
-    private Integer rearRightWheelArchThickness;
-    private Integer frontBumperThickness;
-    private Integer rearBumperThickness;
+    private Integer minHoodThickness;
+    private Integer maxHoodThickness;
+    private Integer minRoofThickness;
+    private Integer maxRoofThickness;
+    private Integer minFrontLeftDoorThickness;
+    private Integer maxFrontLeftDoorThickness;
+    private Integer minRearLeftDoorThickness;
+    private Integer maxRearLeftDoorThickness;
+    private Integer minFrontRightDoorThickness;
+    private Integer maxFrontRightDoorThickness;
+    private Integer minRearRightDoorThickness;
+    private Integer maxRearRightDoorThickness;
+    private Integer minTrunkThickness;
+    private Integer maxTrunkThickness;
+    private Integer minPillarALeftThickness;
+    private Integer maxPillarALeftThickness;
+    private Integer minPillarARightThickness;
+    private Integer maxPillarARightThickness;
+    private Integer minPillarBLeftThickness;
+    private Integer maxPillarBLeftThickness;
+    private Integer minPillarBRightThickness;
+    private Integer maxPillarBRightThickness;
+    private Integer minPillarCLeftThickness;
+    private Integer maxPillarCLeftThickness;
+    private Integer minPillarCRightThickness;
+    private Integer maxPillarCRightThickness;
+    private Integer minFrontLeftWheelArchThickness;
+    private Integer maxFrontLeftWheelArchThickness;
+    private Integer minFrontRightWheelArchThickness;
+    private Integer maxFrontRightWheelArchThickness;
+    private Integer minRearLeftWheelArchThickness;
+    private Integer maxRearLeftWheelArchThickness;
+    private Integer minRearRightWheelArchThickness;
+    private Integer maxRearRightWheelArchThickness;
+    private Integer minFrontBumperThickness;
+    private Integer maxFrontBumperThickness;
+    private Integer minRearBumperThickness;
+    private Integer maxRearBumperThickness;
 
-    @AssertTrue(message = "{paint.not.null.message}")
-    public boolean isThicknessValid() {
+
+    @AssertTrue(message = "{paint.not.blank.message}")
+    public boolean isMinMaxValid() {
         if (noThicknessMeasurements) {
             return true;
         }
 
-        return hoodThickness != null &&
-                roofThickness != null &&
-                frontLeftDoorThickness != null &&
-                rearLeftDoorThickness != null &&
-                frontRightDoorThickness != null &&
-                rearRightDoorThickness != null &&
-                trunkThickness != null &&
-                pillarALeftThickness != null &&
-                pillarARightThickness != null &&
-                pillarBLeftThickness != null &&
-                pillarBRightThickness != null &&
-                pillarCLeftThickness != null &&
-                pillarCRightThickness != null &&
-                frontLeftWheelArchThickness != null &&
-                frontRightWheelArchThickness != null &&
-                rearLeftWheelArchThickness != null &&
-                rearRightWheelArchThickness != null &&
-                frontBumperThickness != null &&
-                rearBumperThickness != null;
+        return isMinMaxCorrect(minHoodThickness, maxHoodThickness) &&
+                isMinMaxCorrect(minRoofThickness, maxRoofThickness) &&
+                isMinMaxCorrect(minFrontLeftDoorThickness, maxFrontLeftDoorThickness) &&
+                isMinMaxCorrect(minRearLeftDoorThickness, maxRearLeftDoorThickness) &&
+                isMinMaxCorrect(minFrontRightDoorThickness, maxFrontRightDoorThickness) &&
+                isMinMaxCorrect(minRearRightDoorThickness, maxRearRightDoorThickness) &&
+                isMinMaxCorrect(minTrunkThickness, maxTrunkThickness) &&
+                isMinMaxCorrect(minPillarALeftThickness, maxPillarALeftThickness) &&
+                isMinMaxCorrect(minPillarARightThickness, maxPillarARightThickness) &&
+                isMinMaxCorrect(minPillarBLeftThickness, maxPillarBLeftThickness) &&
+                isMinMaxCorrect(minPillarBRightThickness, maxPillarBRightThickness) &&
+                isMinMaxCorrect(minPillarCLeftThickness, maxPillarCLeftThickness) &&
+                isMinMaxCorrect(minPillarCRightThickness, maxPillarCRightThickness) &&
+                isMinMaxCorrect(minFrontLeftWheelArchThickness, maxFrontLeftWheelArchThickness) &&
+                isMinMaxCorrect(minFrontRightWheelArchThickness, maxFrontRightWheelArchThickness) &&
+                isMinMaxCorrect(minRearLeftWheelArchThickness, maxRearLeftWheelArchThickness) &&
+                isMinMaxCorrect(minRearRightWheelArchThickness, maxRearRightWheelArchThickness) &&
+                isMinMaxCorrect(minFrontBumperThickness, maxFrontBumperThickness) &&
+                isMinMaxCorrect(minRearBumperThickness, maxRearBumperThickness);
+    }
+
+    private boolean isMinMaxCorrect(Integer min, Integer max) {
+        if (min == null || max == null) return false;
+        return min <= max;
     }
 }
