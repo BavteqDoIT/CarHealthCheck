d19fcff5db1fb29c34cd0f1e318590637882b0ba
BavteqDoIT <bartek.adamek@onet.pl>
2025-09-26 17:38:47 +0200

    Added possibility to login by username or email

--- src/main/java/bavteqdoit/carhealthcheck/service/CustomUserDetailsService.java
+++ src/main/java/bavteqdoit/carhealthcheck/service/CustomUserDetailsService.java
@@ -14,12 +14,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username)
-            throws UsernameNotFoundException {
-
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() ->
-                        new UsernameNotFoundException("User not found"));
+    public UserDetails loadUserByUsername(String login)  throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(login, login)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
         return org.springframework.security.core.userdetails.User
                 .withUsername(user.getUsername())
